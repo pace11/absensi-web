4f57b51f1ee91ec8c55631612e46a666b0280e7f
pace11 <ryanjoker87@gmail.com>
2025-04-08 08:23:58 +0900

    feat: update drizzle

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import { defineConfig } from 'drizzle-kit'
 
-dotenv.config()
+// dotenv.config()
 
 export default defineConfig({
   out: './lib/drizzle',
