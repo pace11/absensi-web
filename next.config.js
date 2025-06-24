aabe023077999095788769b5f696a5b838ca8f58
pace11 <ryanjoker87@gmail.com>
2025-06-25 03:08:37 +0700

    chore: update package version

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
-/** @type {import('next').NextConfig} */
-
-const { version } = require('./package.json')
+import withTM from 'next-transpile-modules'
+const withTranspile = withTM([
+  'rc-util',
+  'rc-picker',
+  'rc-tree',
+  'rc-table',
+  'rc-field-form',
+  'rc-dialog',
+  'rc-motion',
+  'rc-virtual-list',
+  'antd',
+])
 
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_APP_VERSION: version,
-  },
+  productionBrowserSourceMaps: false,
   images: {
     domains: ['res.cloudinary.com'],
   },
 }
 
-module.exports = nextConfig
+export default withTranspile(nextConfig)
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { ProfileContextProvider } from '@/context/profileContextProvider'
-import 'antd/dist/antd.css'
+import { ConfigProvider } from 'antd'
 import '@/styles/globals.css'
 import 'dayjs/locale/id'
 import Layout from '@/layout'
@@ -7,12 +7,14 @@ import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider>
-      <ProfileContextProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ProfileContextProvider>
-    </SessionProvider>
+    <ConfigProvider>
+      <SessionProvider>
+        <ProfileContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ProfileContextProvider>
+      </SessionProvider>
+    </ConfigProvider>
   )
 }
